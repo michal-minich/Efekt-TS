420fc6d5f64b6dd43149539b1d31d69240f890dd
Michal Minich <michal.minich@gmail.com>
2014-04-12 17:22:14 +0200

    formating

--- Sources/visitor.ts
+++ Sources/visitor.ts
@@ -8,9 +8,9 @@ interface AstVisitor<T> {
     visitExpList (el : ExpList) : T;
 
     // statements
-    visitLoop(l : Loop) : T;
-    visitBreak(b : Break) : T;
-    visitContinue(c : Continue) : T;
+    visitLoop (l : Loop) : T;
+    visitBreak (b : Break) : T;
+    visitContinue (c : Continue) : T;
     visitReturn (r : Return) : T;
     visitThrow (th : Throw) : T;
     visitTry (tr : Try) : T;
@@ -24,7 +24,7 @@ interface AstVisitor<T> {
     visitIdent (i : Ident) : T;
     visitMember (m : Member) : T;
     visitFnApply (fna : FnApply) : T;
-    visitBinOpApply (opa : BinOpApply) : T;
+    visitBinOpApply(opa : BinOpApply) : T;
     visitIf (i : If) : T;
     visitNew (nw : New) : T;
     visitTypeOf (tof : TypeOf) : T;
@@ -32,28 +32,28 @@ interface AstVisitor<T> {
     // values
     visitErr (er : Err) : T;
     visitVoid (vo : Void) : T;
-    visitBool(b : Bool) : T;
+    visitBool (b : Bool) : T;
     visitInt (ii : Int) : T;
     visitFloat (f : Float) : T;
     visitChar (ch : Char) : T;
-    visitArr(arr : Arr) : T;
-    visitRef(rf : Ref) : T;
+    visitArr (arr : Arr) : T;
+    visitRef (rf : Ref) : T;
 
     // values / types
     visitFn (fn : Fn) : T;
 
     // types (user defined)
     visitStruct (st : Struct) : T;
-    visitInterface(ifc : Interface) : T;
+    visitInterface (ifc : Interface) : T;
 
     // types (built in)
-    visitTypeAny(ta : TypeAny) : T;
-    visitTypeAnyOf(tao : TypeAnyOf) : T;
-    visitTypeErr(ter : TypeErr) : T;
-    visitTypeVoid(tvo : TypeVoid) : T;
-    visitTypeBool(tb : TypeBool) : T;
-    visitTypeInt(tii : TypeInt) : T;
-    visitTypeFloat(tf : TypeFloat) : T;
-    visitTypeArr(tarr : TypeArr) : T;
-    visitTypeRef(trf : TypeRef) : T;
+    visitTypeAny (ta : TypeAny) : T;
+    visitTypeAnyOf (tao : TypeAnyOf) : T;
+    visitTypeErr (ter : TypeErr) : T;
+    visitTypeVoid (tvo : TypeVoid) : T;
+    visitTypeBool (tb : TypeBool) : T;
+    visitTypeInt (tii : TypeInt) : T;
+    visitTypeFloat (tf : TypeFloat) : T;
+    visitTypeArr (tarr : TypeArr) : T;
+    visitTypeRef (trf : TypeRef) : T;
 }
